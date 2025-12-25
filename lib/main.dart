2826d7d79fc664ce1b2179dc9c4ab132258f738e
Meas Sophanith <sophanithmeas91@gmail.com>
2025-12-25 23:58:25 +0700

    Modify the navigation bar to using a custom widget Update the code structure, future commit should be done in new branches

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:khmer_text_vectorization/app_theme.dart';
 import 'package:khmer_text_vectorization/data/app_database.dart';
+import 'package:khmer_text_vectorization/ui/Screens/vectorize_screen.dart';
 import './ui/navigation.dart';
-import './ui/searchBar.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,10 +18,35 @@ void main() async {
   runApp(MyApp(dictionary: wordsList.toSet()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key, required this.dictionary});
   final Set<Characters> dictionary;
-  // This widget is the root of your application.
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ScreenType currentScreen = ScreenType.dashboard;
+  void onTabClick(ScreenType newScreen) {
+    setState(() {
+      currentScreen = newScreen;
+    });
+  }
+
+  String get appBarTitle {
+    switch (currentScreen) {
+      case ScreenType.dashboard:
+        return "Dashboard";
+      case ScreenType.vectorize:
+        return "Vectorize Text";
+      case ScreenType.collection:
+        return "Corpus Collection";
+      case ScreenType.setting:
+        return "Settings";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,62 +54,24 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       home: Scaffold(
-        appBar: AppBar(title: Text("Vectorize Text")),
-        body: Wizard(),
+        appBar: AppBar(title: Text(appBarTitle)),
+        body: IndexedStack(
+          index: currentScreen.index,
+          children: [
+            Placeholder(),
+            VectorizeScreen(),
+            Placeholder(),
+            Placeholder(),
+          ],
+        ),
+
+        bottomNavigationBar: SafeArea(
+          child: Navigation(
+            currentScreen: currentScreen,
+            onTabClick: onTabClick,
+          ),
+        ),
       ),
-    );
-  }
-}
-
-class Wizard extends StatefulWidget {
-  const Wizard({super.key});
-
-  @override
-  State<Wizard> createState() => _WizardState();
-}
-
-class _WizardState extends State<Wizard> {
-  int currentStep = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Stepper(
-      type: StepperType.horizontal,
-
-      steps: [
-        Step(
-          isActive: currentStep == 0,
-          label: Text(
-            "Text Input",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          title: const SizedBox.shrink(),
-          content: Text("What do you want to know?"),
-        ),
-        Step(
-          isActive: currentStep == 1,
-          title: const SizedBox.shrink(),
-          label: Text(
-            "Segmentation",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          content: Text("What do you want to know?"),
-        ),
-        Step(
-          isActive: currentStep == 2,
-          title: const SizedBox.shrink(),
-          label: Text(
-            "Data Labelling",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          content: Text("What do you want to know?"),
-        ),
-      ],
-      onStepTapped: (value) {
-        setState(() {
-          currentStep = value;
-        });
-      },
-      currentStep: currentStep,
     );
   }
 }
